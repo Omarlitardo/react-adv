415d4140a2aaf27f85a98ed1e3098a534f2dd757
omar <omar_litardo@hotmail.com>
2024-07-07 23:25:48 -0500

    Proyecto inicial listo

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,53 @@
+import { BrowserRouter, Navigate } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
+import logo from "../assets/react.svg";
+
+export const Navigation = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React Logo" />
+            <ul>
+              <li>
+                <NavLink
+                  to="home"
+                  className={({ isActive }) => (isActive ? "nav-active" : "")}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="about"
+                  className={({ isActive }) => (isActive ? "nav-active" : "")}
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="users"
+                  className={({ isActive }) => (isActive ? "nav-active" : "")}
+                >
+                  Users
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="about" element={<h1>About Page</h1>} />
+            <Route path="users" element={<h1>User Page</h1>} />
+            <Route path="home" element={<h1>Home Page</h1>} />
+
+            <Route path="/*" element={<Navigate to="/home" replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+};
+
+// instalar en terminal,o no funcionara los Router..
+// npm add  react-router-dom@6
